3b0245c1cb494cea0ed5fee67f71292666853c6f
Lorenz Sieben <me@lorenz-sieben.com>
2024-06-11 16:01:08 +0200

    Implement suggestions from code review

--- assets/js/trackhar.tsx
+++ assets/js/trackhar.tsx
@@ -3,7 +3,7 @@ import { baseUrl } from '@params';
 import { Buffer } from 'buffer';
 import type { Har } from 'har-format';
 import { render } from 'preact';
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import { processRequest, unhar, type AnnotatedResult, type IndicatorValues, type Request } from 'trackhar';
 import { getFragmentParams } from './util/browser';
 import { t as _t } from './util/i18n';
@@ -11,11 +11,14 @@ import { t as _t } from './util/i18n';
 const t = (key: string) => _t('trackhar-ui-' + key, 'hugo');
 const absUrl = (path: string) => baseUrl + path;
 
+type RequestState = { request: Request; result: AnnotatedResult | undefined };
+
 const App = () => {
     const [hideUnmatched, setHideUnmatched] = useState<boolean>(true);
     const [error, setError] = useState<string>();
-    const [requests, setRequests] = useState<{ request: Request; result: AnnotatedResult | undefined }[]>();
+    const [requests, setRequests] = useState<RequestState[]>();
     const [indicators, setIndicators] = useState<IndicatorValues>();
+    const indicatorFileElement = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         const fragmentParams = getFragmentParams();
@@ -42,6 +45,21 @@ const App = () => {
             }
         })();
     }, []);
+
+    useEffect(() => {
+        if (!requests) return;
+
+        try {
+            const result = requests.map((r) => ({
+                request: r.request,
+                result: processRequest(r.request, { indicatorValues: indicators }),
+            }));
+            setRequests(result);
+        } catch (err) {
+            console.error('Parsing failed:', err);
+            setError(t('parsing-failed-request'));
+        }
+    }, [indicators]);
 
     const filteredRequests = requests?.filter((r) => !hideUnmatched || r.result);
 
@@ -86,7 +104,7 @@ const App = () => {
                 {t('hide-unmatched')}
             </label>
 
-            <details style="padding: 10px;">
+            <details style="padding: 10px; margin-top: 10px;">
                 <summary>{t('indicators-heading')}</summary>
 
                 {t('indicators-explanation')}
@@ -104,6 +122,7 @@ const App = () => {
                         type="file"
                         accept=".json,application/json"
                         multiple={false}
+                        ref={indicatorFileElement}
                         onChange={async (e) => {
                             const file = e.currentTarget.files?.[0];
                             if (!file) return;
@@ -113,14 +132,6 @@ const App = () => {
                             try {
                                 const indicators: IndicatorValues = JSON.parse(await file.text());
                                 setIndicators(indicators);
-
-                                if (!requests) return;
-
-                                const result = requests.map((r) => ({
-                                    request: r.request,
-                                    result: processRequest(r.request, { indicatorValues: indicators }),
-                                }));
-                                setRequests(result);
                             } catch (error) {
                                 console.error('Parsing indicators failed:', error);
                                 setError(t('parsing-failed-indicators'));
@@ -128,135 +139,143 @@ const App = () => {
                         }}
                     />
                 </label>
+
+                <label>
+                    <button
+                        onClick={() => {
+                            setIndicators(undefined);
+                            if (indicatorFileElement.current) indicatorFileElement.current.value = '';
+                        }}>
+                        {t('remove-indicators')}
+                    </button>
+                </label>
             </details>
 
             <div>
                 {error ? (
-                    <em>{error}</em>
+                    <blockquote class="book-hint danger">{error}</blockquote>
                 ) : !filteredRequests ? (
                     <></>
                 ) : filteredRequests.length === 0 ? (
-                    <em>{hideUnmatched ? t('no-matching-requests') : t('no-requests')}</em>
+                    <blockquote class="book-hint info">
+                        {hideUnmatched ? t('no-matching-requests') : t('no-requests')}
+                    </blockquote>
                 ) : (
-                    filteredRequests.map(({ request, result }) => {
-                        const adapter = result?.[0]?.adapter;
-
-                        return (
-                            <>
-                                <h2>
-                                    <code style="word-break: break-all;">
-                                        <strong>{request.method}</strong> {request.scheme}://{request.host}
-                                        {request.path}
-                                    </code>
-                                </h2>
-
-                                {result ? (
-                                    <>
-                                        <em>
-                                            {adapter === 'indicators' ? (
-                                                t('matched-by-indicators')
-                                            ) : (
-                                                <>
-                                                    {t('matched-by-adapter')}{' '}
-                                                    <a href={absUrl('t/' + adapter)} target="_blank">
-                                                        {adapter}
-                                                    </a>
-                                                </>
-                                            )}
-                                        </em>
-
-                                        <table class="data-table">
-                                            <thead>
-                                                <tr>
-                                                    <th style="width: 25%;">
-                                                        {_t(
-                                                            'tracker-single-transmitted-data-table-head-property',
-                                                            'hugo'
-                                                        )}
-                                                    </th>
-                                                    <th style="width: 10%;">
-                                                        {_t(
-                                                            'tracker-single-transmitted-data-table-head-context',
-                                                            'hugo'
-                                                        )}
-                                                    </th>
-                                                    <th style="width: 25%;">
-                                                        {_t('tracker-single-transmitted-data-table-head-path', 'hugo')}
-                                                    </th>
-                                                    <th style="width: 40%;">{t('value')}</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {result.map((r) => (
-                                                    <tr>
-                                                        <td>{_t(r.property as string, 'properties')}</td>
-                                                        <td>{r.context}</td>
-                                                        <td>
-                                                            <code>
-                                                                {r.path}{' '}
-                                                                {r.reasoning.startsWith('https://') ||
-                                                                r.reasoning.startsWith('http://') ? (
-                                                                    <a
-                                                                        href={r.reasoning}
-                                                                        rel="nofollow"
-                                                                        target="_blank">
-                                                                        <img
-                                                                            src="/svg/external.svg"
-                                                                            class="inline-icon icon-external"
-                                                                            alt={_t(
-                                                                                'tracker-single-transmitted-data-table-reasoning-external-link-desc',
-                                                                                'hugo'
-                                                                            )}
-                                                                            title={_t(
-                                                                                'tracker-single-transmitted-data-table-reasoning-external-link-desc',
-                                                                                'hugo'
-                                                                            )}
-                                                                        />
-                                                                    </a>
-                                                                ) : r.reasoning.endsWith('.md') ? (
-                                                                    <a
-                                                                        href={absUrl(
-                                                                            'research/' +
-                                                                                r.reasoning
-                                                                                    .replace(/\/([^/]+)$/, '#$1')
-                                                                                    .replace(/.md$/, '')
-                                                                        )}
-                                                                        target="_blank">
-                                                                        <img
-                                                                            src="/svg/information.svg"
-                                                                            class="inline-icon icon-information"
-                                                                            title={_t(
-                                                                                'tracker-single-transmitted-data-table-reasoning-link-desc',
-                                                                                'hugo'
-                                                                            )}
-                                                                            alt={_t(
-                                                                                'tracker-single-transmitted-data-table-reasoning-link-desc',
-                                                                                'hugo'
-                                                                            )}
-                                                                        />
-                                                                    </a>
-                                                                ) : (
-                                                                    <></>
-                                                                )}
-                                                            </code>
-                                                        </td>
-                                                        <td>
-                                                            <code>{r.value}</code>
-                                                        </td>
-                                                    </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                    </>
-                                ) : (
-                                    <em>{t('unsupported-endpoint')}</em>
-                                )}
-                            </>
-                        );
-                    })
+                    filteredRequests.map((r) => <RequestView {...r} />)
                 )}
             </div>
         </>
+    );
+};
+
+const RequestView = ({ request, result }: RequestState) => {
+    const adapter = result?.[0]?.adapter;
+
+    return (
+        <>
+            <h2>
+                <code style="word-break: break-all;">
+                    <strong>{request.method}</strong> {request.scheme}://{request.host}
+                    {request.path}
+                </code>
+            </h2>
+
+            {Array.isArray(result) ? (
+                result.length === 0 ? (
+                    <blockquote class="book-hint">{t('matched-no-data')}</blockquote>
+                ) : (
+                    <>
+                        <blockquote class="book-hint info">
+                            {adapter === 'indicators' ? (
+                                t('matched-by-indicators')
+                            ) : (
+                                <>
+                                    {t('matched-by-adapter')}{' '}
+                                    <a href={absUrl('t/' + adapter)} target="_blank">
+                                        {adapter}
+                                    </a>
+                                </>
+                            )}
+                        </blockquote>
+                        <div class="data-table-container">
+                            <table class="data-table">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 25%;">
+                                            {_t('tracker-single-transmitted-data-table-head-property', 'hugo')}
+                                        </th>
+                                        <th style="width: 10%;">
+                                            {_t('tracker-single-transmitted-data-table-head-context', 'hugo')}
+                                        </th>
+                                        <th style="width: 25%;">
+                                            {_t('tracker-single-transmitted-data-table-head-path', 'hugo')}
+                                        </th>
+                                        <th style="width: 40%;">{t('value')}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {result.map((r) => (
+                                        <ResultRow result={r} />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+                )
+            ) : (
+                <blockquote class="book-hint">{t('unsupported-endpoint')}</blockquote>
+            )}
+        </>
+    );
+};
+
+const ResultRow = ({ result }: { result: AnnotatedResult[number] }) => {
+    let propertyName;
+
+    try {
+        propertyName = _t(result.property as string, 'properties');
+    } catch (e) {
+        propertyName = <code>{result.property as string}</code>;
+    }
+
+    return (
+        <tr>
+            <td>{propertyName}</td>
+            <td>{result.context}</td>
+            <td>
+                <code>
+                    {result.path}{' '}
+                    {result.reasoning.startsWith('https://') || result.reasoning.startsWith('http://') ? (
+                        <a href={result.reasoning} rel="nofollow" target="_blank">
+                            <img
+                                src="/svg/external.svg"
+                                class="inline-icon icon-external"
+                                alt={_t('tracker-single-transmitted-data-table-reasoning-external-link-desc', 'hugo')}
+                                title={_t('tracker-single-transmitted-data-table-reasoning-external-link-desc', 'hugo')}
+                            />
+                        </a>
+                    ) : result.reasoning.endsWith('.md') ? (
+                        <a
+                            href={absUrl(
+                                'research/' + result.reasoning.replace(/\/([^/]+)$/, '#$1').replace(/\.md$/, '')
+                            )}
+                            target="_blank">
+                            <img
+                                src="/svg/information.svg"
+                                class="inline-icon icon-information"
+                                title={_t('tracker-single-transmitted-data-table-reasoning-link-desc', 'hugo')}
+                                alt={_t('tracker-single-transmitted-data-table-reasoning-link-desc', 'hugo')}
+                            />
+                        </a>
+                    ) : (
+                        <></>
+                    )}
+                </code>
+            </td>
+            <td class="value-column">
+                <code>{result.value}</code>
+            </td>
+        </tr>
     );
 };
 
--- content/en/trackhar.md
+++ content/en/trackhar.md
@@ -8,4 +8,6 @@
 
 [TrackHAR](https://github.com/tweaselORG/TrackHAR) is our library to extract transmitted data from tracking requests in mobile apps. Here, you can use TrackHAR online without having to download anything. The processing happens entirely locally in your browser—your files are never uploaded to our server.
 
-<div id="trackhar-app"></div>
+<div id="trackhar-app">
+<noscript>JavaScript is not enabled. Because the processing happens only in your browser, you need to enable JavaScript to use the tool. Your data will remain on your device the whole time.</noscript>
+</div>
