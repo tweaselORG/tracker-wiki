c7902f7e97824d20ac17f878b5a4509b5aaf3981
Lorenz Sieben <me@lorenz-sieben.com>
2024-06-11 21:18:11 +0200

    Fixes #15: Automatically generate example values on build (#24)
    * Implement new example data format
    
    * Automatically generate example data on build
    
    * Implement changes from code review

--- scripts/copyResearchDocs.ts
+++ scripts/copyResearchDocs.ts
@@ -1,7 +1,10 @@
 import { execa } from 'execa';
-import { copyFile, cp, mkdir, readdir, rm, writeFile } from 'fs/promises';
+import { copyFile, cp, mkdir, readFile, readdir, rm, stat, writeFile } from 'fs/promises';
 import { join } from 'path';
 import { adapters, type Tracker } from 'trackhar';
+
+type TrackharVersionFile = { trackharReleaseTag: string };
+const trackharVersionFile = 'data/trackharVersion.json';
 
 /**
  * Copies the research documentation from TrackHAR. Since we do not translate the research docs (yet), we only copy them
@@ -19,6 +22,20 @@ import { adapters, type Tracker } from 'trackhar';
         '--no-progress',
         '--json',
     ]).then(({ stdout }) => JSON.parse(stdout).data.trees[0].name.split('@')[1] as string);
+
+    if (!process.env['FORCE_REGENERATE'] && (await stat(trackharVersionFile))) {
+        const currentVersion = await readFile(trackharVersionFile).then(
+            (res) => JSON.parse(res.toString()) as TrackharVersionFile
+        );
+        if (currentVersion.trackharReleaseTag === `v${trackharVersion}`) {
+            // If we already copied the docs for the current version of TrackHAR, we don’t need to run this again.
+            console.info('Version was generated already, skipping…');
+            return;
+        }
+    }
+
+    // Remove any old data before cloning again.
+    await rm('trackhar_tmp', { recursive: true, force: true });
 
     console.info(`Fetching data for TrackHAR version: ${trackharVersion}`);
     await execa('git', [
@@ -59,9 +76,16 @@ import { adapters, type Tracker } from 'trackhar';
     );
     await cp('trackhar_tmp/research-docs', 'content/en/research', { recursive: true });
 
+    console.info('Generate new example data…');
+    await execa('yarn', [], { cwd: 'trackhar_tmp' });
+    // If the server is too slow, we should timeout after 10 minutes
+    await execa('yarn', ['tsx', 'scripts/generate-example-data.ts'], { cwd: 'trackhar_tmp', timeout: 600000 });
+    await copyFile('trackhar_tmp/research-docs/adapter-examples.json', 'data/adapterExamples.json');
+
+    console.info('Cleanup…');
     await rm('trackhar_tmp', { recursive: true, force: true });
 
-    await writeFile('data/trackharVersion.json', JSON.stringify({ trackharReleaseTag: `v${trackharVersion}` }));
+    await writeFile(trackharVersionFile, JSON.stringify({ trackharReleaseTag: `v${trackharVersion}` }));
 })();
 
 /* eslint-enable no-console */
